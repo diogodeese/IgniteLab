b6c63bc667ce5f2361f9c56ec35152915e2a1e0f
Diogo Santos <diogodeese@gmail.com>
2022-06-26 00:14:51 +0100

    Added a link to the header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import { Logo } from "./Logo";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      <Logo />
+      <Link to="/" className="ml-8 md:ml-0">
+        <Logo />
+      </Link>
     </header>
   );
 }
